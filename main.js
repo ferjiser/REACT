0a8c86bc2809c63d3b2ce20968749d60287e37c0
Fernando Jimenez <fernando.jimenezserrano@amaris.com>
2017-08-10 09:59:19 +0200

    Creating components

--- main.js
+++ main.js
@@ -1,20 +1,62 @@
-function HelloWorld(props){
-    const name = props.name;
+class MyComponent extends React.Component {
 
-    return (
-        <h1 id="Title">Hola {name}</h1>
-    );
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+            count: 0
+
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+       this.setState({
+           count: this.state.count + 1
+       });
+    }
+
+    render () {
+        const name = this.props.name;
+        return (
+            <div>
+                <h1 id="Title">Hola {name} {this.state.count}</h1>
+                <button type="button" onClick = {this.handleClick}>Click Me!</button>
+            </div>
+        );
+    }
+
 }
 
+const OtherComponent = React.createClass({
+
+    getInitialState(){
+        return {
+            count : 0
+        }
+    },
+
+    handleClick(){
+        this.setState({
+            count: this.state.count + 1
+        });
+    },
+
+    render () {
+        const name = this.props.name;
+        return (
+            <div>
+                <h1 id="Title">Hola {name} {this.state.count}</h1>
+                <button type="button" onClick = {this.handleClick}>Click Me!</button>
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <HelloWorld name="Fernando JSX"/>,
+    <MyComponent name="Componente"/>,
+    //<OtherComponent name="Componente"/>,
     document.getElementById('app')
 );
-
-setTimeout(
-    () => {
-    ReactDOM.render(
-        <HelloWorld name="Fernando Jiménez JSX"/>,
-        document.getElementById('app')
-    )
-}, 3000);
