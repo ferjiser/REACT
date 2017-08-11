635e4d4a8b15646c2a879c6ee642e7165082a776
Fernando Jimenez <fernando.jimenezserrano@amaris.com>
2017-08-11 13:52:45 +0200

    Finished chapter

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-class MyComponent extends React.Component {
+function PureComponent(props){
+
+    const name = props.name;
+
+    return (
+        <div>
+            <h1 id="Title">Hola {name} {props.count}</h1>
+            <button type="button" onClick = {props.onClick}>Click Me!</button>
+        </div>
+    );
+}
+
+
+class ContainerComponent extends React.Component {
 
     constructor(props){
 
@@ -12,6 +25,37 @@ class MyComponent extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
 
+    componentWillMount(){
+        console.log('El componente se va a montar');
+    }
+
+    componentDidMount(){
+        console.log('El componente ya está montado');
+    }
+
+    componentWillReceiveProps(nextProps){
+        console.log('Si las propiedades cambian reseteo');
+        this.setState({
+            count: 0
+        });
+    }
+
+    componentWillUpdate(){
+        console.log('El componente se va a actualizar');
+    }
+
+    componentDidUpdate(){
+        console.log('El componente se actualizó');
+    }
+
+    componentWillUnmount(){
+        console.log('El componente se desmontará');
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextState.count !== this.state.count;
+    }
+
     handleClick(){
        this.setState({
            count: this.state.count + 1
@@ -19,44 +63,18 @@ class MyComponent extends React.Component {
     }
 
     render () {
-        const name = this.props.name;
         return (
-            <div>
-                <h1 id="Title">Hola {name} {this.state.count}</h1>
-                <button type="button" onClick = {this.handleClick}>Click Me!</button>
-            </div>
+            <PureComponent
+                name = {this.props.name}
+                count = {this.state.count}
+                onClick = {this.handleClick}/>
         );
     }
 
 }
 
-const OtherComponent = React.createClass({
-
-    getInitialState(){
-        return {
-            count : 0
-        }
-    },
-
-    handleClick(){
-        this.setState({
-            count: this.state.count + 1
-        });
-    },
-
-    render () {
-        const name = this.props.name;
-        return (
-            <div>
-                <h1 id="Title">Hola {name} {this.state.count}</h1>
-                <button type="button" onClick = {this.handleClick}>Click Me!</button>
-            </div>
-        );
-    }
-});
 
 ReactDOM.render(
-    <MyComponent name="Componente"/>,
-    //<OtherComponent name="Componente"/>,
+    <ContainerComponent name="Componente"/>,
     document.getElementById('app')
 );
