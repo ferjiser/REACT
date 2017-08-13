8eb22a7af86fe2c68b3822f57e1a9a21739055b4
Fernando Jimenez <Fernando Jimenez>
2017-08-13 14:20:29 +0200

    Finished charpter

--- main.js
+++ main.js
@@ -1,3 +1,43 @@
+function HocComponent(WrappedComponent) {
+
+    class Hoc extends React.Component {
+
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                count: 0
+
+            };
+
+            this.handleClick = this
+                .handleClick
+                .bind(this);
+
+        }
+
+        getChildContext() {
+            return {count: this.state.count};
+        }
+
+        handleClick() {
+            this.setState({
+                count: this.state.count + 1
+            });
+        }
+
+        render() {
+            return (<WrappedComponent onClick={this.handleClick} {...this.props}/>)
+        }
+    }
+
+    Hoc.childContextTypes = {
+        count: React.PropTypes.number
+    }
+
+    return Hoc;
+}
+
 function PureComponent(props, context) {
 
     const name = props.name;
@@ -15,73 +55,23 @@ PureComponent.contextTypes = {
     count: React.PropTypes.number
 };
 
-class ContainerComponent extends React.Component {
+const PureComponentHOC = HocComponent(PureComponent);
 
-    constructor(props) {
-
-        super(props);
-
-        this.state = {
-            count: 0
-
-        };
-
-        this.handleClick = this
-            .handleClick
-            .bind(this);
-    }
-
-    getChildContext() {
-        return {count: this.state.count}
-    }
-
-    componentWillMount() {
-        console.log('El componente se va a montar');
-    }
-
-    componentDidMount() {
-        console.log('El componente ya está montado');
-    }
-
-    componentWillReceiveProps(nextProps) {
-        console.log('Si las propiedades cambian reseteo');
-        this.setState({count: 0});
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState.count !== this.state.count;
-    }
-
-    componentWillUpdate() {
-        console.log('El componente se va a actualizar');
-    }
-
-    componentDidUpdate() {
-        console.log('El componente se actualizó');
-    }
-
-    componentWillUnmount() {
-        console.log('El componente se desmontará');
-    }
-
+const mixin = {
     handleClick() {
-        this.setState({
-            count: this.state.count + 1
-        });
+        console.log('Clickeado');
     }
-
-    render() {
-        return (<PureComponent
-            name={this.props.name}
-            count={this.state.count}
-            onClick={this.handleClick}/>);
-    }
-
 }
 
-ContainerComponent.childContextTypes = {
-    count: React.PropTypes.number
-};
+const MixinComponent = React.createClass({
+    mixins: [mixin],
+    render() {
+        return <div onClick={this.handleClick}>Hola Mixin Component</div>
+
+    }
+});
 
 ReactDOM.render(
-    <ContainerComponent name="Componente"/>, document.getElementById('app'));
+    <PureComponentHOC name="Componente HOC"/>, document.getElementById('hoc'));
+ReactDOM.render(
+    <MixinComponent name="Componente Mixin"/>, document.getElementById('mixin'));
